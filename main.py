d5f28bfa30bdfa4d4bc8b3980d84fdaa5a377621
Jaeu Jeong <wodndb@gmail.com>
2023-11-06 15:56:47 +0900

    test html2img

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from typing import Union
-
 from fastapi import FastAPI
+from html2image import Html2Image
 
+hti = Html2Image(size=(500, 200))
 app = FastAPI()
+
 
 
 @app.get("/")
@@ -12,4 +14,8 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    html = """<h1> An interesting title </h1> This page will be red"""
+    css = "body {background: red;}"
+
+    hti.screenshot(html_str=html, css_str=css, save_as='red_page.png')
     return {"item_id": item_id, "q": q} 
